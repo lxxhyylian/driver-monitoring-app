b5c08d7aeb382b53f5f8ca7e7cac53ee6ddd1a5d
lxxhyylian <hyylinh2912@gmail.com>
2025-09-23 00:46:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def crop_face_mediapipe(img, crop_size=256, padding_ratio=0.4):
         box = det.location_data.relative_bounding_box
         x, y, bw, bh = box.xmin, box.ymin, box.width, box.height
         face_area = bw * bh
+        st.write(face_area)
         if face_area >= 0.94:
             return img
         x1 = int((x - padding_ratio * 1.2 * bw) * w)
